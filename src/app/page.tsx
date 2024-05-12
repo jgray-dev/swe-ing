f90ee89f885b6f64316d6b91a8b6842a1b28ed53
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-12 21:10:46 +0000

    Fetch posts from DB and display them alongside mock images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/005ba145-6d2c-45fd-92b8-05cde29662d1-1pyk87.jpg",
   "https://utfs.io/f/95bd0a1b-7235-4690-a7ce-04615a19131f-1pyk89.png",
@@ -9,12 +11,17 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 export default async function Home() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="h-screen w-screen bg-zinc-900 text-white">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post, index) => {
+          return <div key={index}>{post.name}</div>;
+        })}
         {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
-          (image) => (
-            <div key={image.id} className="w-48">
+          (image, index) => (
+            <div key={image.id + "-" + index} className="w-48">
               <img src={image.url} alt={`Image ${image.id}`} />
             </div>
           ),
