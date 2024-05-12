d6ae157f22d54ab26196a86ab34ce0034d6b3b83
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-12 22:02:34 +0000

    chore(schema): Refactor database schema to use multi-project feature

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -10,12 +10,6 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `swe-ing_${name}`);
 
 export const posts = createTable(
@@ -30,5 +24,5 @@ export const posts = createTable(
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
