b8c5123a6e4c773e8c2a86394194559ad63716a7
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-14 19:24:28 +0000

    feat(server): add getPost query to fetch post by id

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,6 +1,22 @@
 import "server-only";
 import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
+import { posts } from "~/server/db/schema";
+import { eq } from "drizzle-orm";
 
 export async function getPosts() {
   return db.query.posts.findMany();
+}
+
+export async function getPost(id: number) {
+  const user = auth();
+
+  if (!user.userId) throw new Error("Unauthorized");
+
+  return await db
+    .select()
+    .from(posts)
+    .where(eq(posts.id, id))
+    .limit(1)
+    .then((result) => result[0]);
 }
