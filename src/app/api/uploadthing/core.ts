1ca7f37cdf3d126572de7c91d1188a8353068ee8
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-14 18:46:26 +0000

    chore: Update `uploadthing/core.ts` to add max file count for image uploader

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,27 +1,18 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { auth, clerkClient } from "@clerk/nextjs/server";
-import { db } from "~/server/db";
-import { posts } from "~/server/db/schema";
-import { metadata } from "~/app/layout";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 3 } })
     .middleware(async () => {
       const user = auth();
       if (!user.userId) throw new UploadThingError("Unauthorized");
       return { user: await clerkClient.users.getUser(`${user.userId}`) };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      await db.insert(posts).values({
-        authorId: `${metadata.user.id}`,
-        imageUrl: file.url,
-        firstName: `${metadata.user.firstName}`,
-        lastName: `${metadata.user.lastName}`,
-      });
-      return { uploadedBy: metadata.user.id };
+      return { url: file.url, userId: `${metadata.user.id}` };
     }),
 } satisfies FileRouter;
 
