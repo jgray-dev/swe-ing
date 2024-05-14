1a3f322332ca22189782bea9c9941f53a6cc8c1f
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-14 03:30:58 +0000

    feat(db): update posts schema with firstName and lastName fields

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -14,8 +14,9 @@ export const createTable = pgTableCreator((name) => `swe-ing_${name}`);
 
 export const posts = createTable("post", {
   id: serial("id").primaryKey(),
-  author: varchar("author", { length: 256 }).notNull(),
   authorId: varchar("author_id").notNull(),
+  firstName: varchar("first_name", { length: 128 }).notNull(),
+  lastName: varchar("last_name", { length: 128 }).notNull(),
   imageUrl: varchar("image_url", { length: 1024 }).notNull(),
   createdAt: timestamp("created_at", { withTimezone: true })
     .default(sql`CURRENT_TIMESTAMP`)
