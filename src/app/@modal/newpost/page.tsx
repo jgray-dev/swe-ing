32b1c6d4070fb70b0cd7d69690c9fa41c559162c
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-14 17:18:27 +0000

    chore: add close button and refactor modal in new post component

--- src/app/@modal/newpost/page.tsx
+++ src/app/@modal/newpost/page.tsx
@@ -1,37 +1,51 @@
 "use client";
 import { UploadDropzone } from "~/utils/uploadthing";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Modal } from "./modal";
+import { useRouter } from "next/navigation";
 
 export default function NewPost() {
+  const router = useRouter();
   const [content, setContent] = useState("");
+
   return (
-    <div
-      className="fixed left-0 top-0 h-screen w-screen scroll-auto bg-black/75 backdrop-blur-sm"
-      onClick={() => {
-        console.log("CLOSE");
-      }}
-    >
+    <Modal>
       <div
-        className={"mx-auto h-screen w-96 text-center"}
-        onClick={(e) => {
-          e.stopPropagation();
+        className="fixed left-0 top-0 h-screen w-screen scroll-auto bg-black/75 backdrop-blur-sm"
+        onClick={() => {
+          router.back();
         }}
       >
-        <h1>Create new post</h1>
-        <textarea
-          className="h-48 w-full rounded-md border border-gray-200 bg-black/90 p-4 text-white placeholder-neutral-200"
-          value={content}
-          placeholder="Write your post here"
-          onChange={(e) => setContent(e.target.value)}
-        />
-        <UploadDropzone endpoint="imageUploader" />
-        <button
-          className="mt-4 w-full rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-          onClick={() => console.log(content)}
+        <div
+          className={"mx-auto h-screen w-96 text-center"}
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
         >
-          Submit
-        </button>
+          <div
+            className="mt-12 w-full cursor-pointer bg-red-500 text-white"
+            onClick={() => {
+              router.back();
+            }}
+          >
+            CLOSE
+          </div>
+          <h1>Create new post</h1>
+          <textarea
+            className="h-48 w-full rounded-md border border-gray-200 bg-black/90 p-4 text-white placeholder-neutral-200"
+            value={content}
+            placeholder="Write your post here"
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <UploadDropzone endpoint="imageUploader" />
+          <button
+            className="mt-4 w-full rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+            onClick={() => console.log(content)}
+          >
+            Submit
+          </button>
+        </div>
       </div>
-    </div>
+    </Modal>
   );
 }
