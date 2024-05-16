f282f8ec70c0c0025b78125b109c7709c1226173
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 02:52:13 +0000

    Refactor new post page to use tRPC and simplify UI

--- src/app/newpost/page.tsx
+++ src/app/newpost/page.tsx
@@ -1,89 +1,43 @@
 "use client";
-import { UploadDropzone } from "~/utils/uploadthing";
-import { useState } from "react";
+
 import { useRouter } from "next/navigation";
-import { auth } from "@clerk/nextjs/server";
-import { db } from "~/server/db";
-import { posts } from "~/server/db/schema";
+import { useState } from "react";
+
+import { api } from "~/trpc/react";
 
 export default function NewPost() {
   const router = useRouter();
-  const [content, setContent] = useState("");
-  const [imageUrls, setImageUrls] = useState<string[]>([]);
-  const [blockSubmit, setBlockSubmit] = useState(false);
+  const [name, setName] = useState("");
 
-  async function handleSubmit() {
-    const user = auth();
-    if (!user.userId) throw new Error("Unauthorized");
-    if (blockSubmit) {
-      alert("Please wait for image upload to complete.");
-    } else if (content !== "") {
-      console.log("Add to database here:");
-      console.log(imageUrls);
-      console.log(content);
-    } else {
-      alert("Please add content to your post");
-    }
-  }
+  const createPost = api.post.create.useMutation({
+    onSuccess: () => {
+      router.refresh();
+      setName("");
+    },
+  });
 
-  function handleImageUpload(response: { url: string }[]) {
-    const urls = response.map((item) => item.url);
-    setImageUrls([...imageUrls, ...urls]);
-    setBlockSubmit(false);
-  }
   return (
-    <div className={"h-screen w-screen"}>
-      <div
-        className="fixed left-0 top-0 h-screen w-screen scroll-auto bg-black/75 backdrop-blur-sm"
-        onClick={() => {
-          router.back();
-        }}
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        createPost.mutate({ name });
+      }}
+      className="flex flex-col gap-2"
+    >
+      <input
+        type="text"
+        placeholder="Title"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="w-full rounded-full px-4 py-2 text-black"
+      />
+      <button
+        type="submit"
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+        disabled={createPost.isPending}
       >
-        <div
-          className={"mx-auto h-screen w-96 text-center"}
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-        >
-          <div
-            className="mt-12 w-full cursor-pointer bg-red-500 text-white"
-            onClick={() => {
-              router.back();
-            }}
-          >
-            CLOSE
-          </div>
-          <h1>Create new post</h1>
-          <textarea
-            className="h-48 w-full rounded-md border border-gray-200 bg-black/90 p-4 text-white placeholder-neutral-200"
-            value={content}
-            placeholder="Write your post here"
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <UploadDropzone
-            endpoint="imageUploader"
-            onBeforeUploadBegin={(files) => {
-              setBlockSubmit(true);
-              return files;
-            }}
-            onUploadError={() => {
-              alert("Error uploading image");
-            }}
-            config={{
-              mode: "auto",
-            }}
-            onClientUploadComplete={(res) => {
-              handleImageUpload(res);
-            }}
-          />
-          <button
-            className={`mt-4 w-full rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 ${blockSubmit ? "cursor-progress" : "cursor-pointer"}`}
-            onClick={() => handleSubmit()}
-          >
-            Submit
-          </button>
-        </div>
-      </div>
-    </div>
+        {createPost.isPending ? "Submitting..." : "Submit"}
+      </button>
+    </form>
   );
 }
