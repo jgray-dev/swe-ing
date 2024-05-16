6157fee137c94a27fc7b8fa6ae755711443355d0
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 19:18:36 +0000

    Refactor PostPage component to use tRPC query and improve error handling

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,31 +1,46 @@
-import { getPost } from "~/server/queries";
-import Link from "next/link";
-import { getLocalTime } from "~/app/_functions/functions";
+"use client";
 
-export default async function PostPage({ params }: { params: { id: string } }) {
-  const postId = params.id;
-  const post = await getPost(Number(postId));
-  console.log(post);
-  if (post) {
-    const time = getLocalTime(post.created_at);
-    console.log(time);
-    return (
-      <div className={"h-full w-full bg-white/10"}>
-        <div>{post.content}</div>
-        This is a post page for post id {postId}
-      </div>
-    );
+import { trpc } from "~/trpc/react";
+import Link from "next/link";
+
+export default function PostPage({ params }: { params: { id: string } }) {
+  const postId = Number(params.id);
+  const post = trpc.post.getSingle.useQuery({ id: postId });
+
+  if (!post.isLoading) {
+    if (post.data) {
+      const time = new Date(post.data?.created_at).toLocaleString();
+      console.log(time);
+      return (
+        <div className={"h-full w-full bg-white/10"}>
+          <div>ahh</div>
+          This is a post page for post id {postId}
+          {post.data?.content}
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className={
+            "left-0 top-0 z-50 h-screen w-screen bg-black text-center text-2xl text-red-400"
+          }
+        >
+          <div>Post not found</div>
+          <div className="mt-4 text-lg text-white underline">
+            <Link href="/">Go back home</Link>
+          </div>
+        </div>
+      );
+    }
   } else {
     return (
       <div
         className={
-          "fixed left-0 top-0 z-50 h-screen w-screen bg-black text-center text-2xl text-red-400"
+          "text-md left-0 top-0 z-50 h-screen w-screen bg-black text-center text-white"
         }
       >
-        <div>Post not found</div>
-        <div className="mt-4 text-lg text-white underline">
-          <Link href="/">Go back home</Link>
-        </div>
+        <div>Loading</div>
+        <div className="mt-4 text-lg text-white underline"></div>
       </div>
     );
   }
