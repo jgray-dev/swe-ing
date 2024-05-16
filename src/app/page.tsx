f72fe34ea072bb84ad5b719692b885bb66152677
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 02:52:14 +0000

    feat(page.tsx): Update background color and text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   const posts = await getPosts();
 
   return (
-    <main className="h-screen w-screen bg-zinc-900 text-white">
+    <main className="h-screen w-screen bg-black/60 text-white">
       <div className="flex flex-wrap gap-4">
         {posts.map((post, index) => {
           console.log(post);
