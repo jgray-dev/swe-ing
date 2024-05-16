63d76701d40ba0effc71e14136c4180edc95df3d
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 17:44:38 +0000

    Refactor timestamp function to use local time format

--- src/app/_functions/functions.ts
+++ src/app/_functions/functions.ts
@@ -1,3 +1,3 @@
-export function getTimestamp(unix: number) {
-  return new Date(unix * 1000).toISOString();
+export function getLocalTime(unix: number) {
+  return new Date(unix * 1000).toLocaleString();
 }
