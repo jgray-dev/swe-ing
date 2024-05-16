f56679d4cb4179ae5bc7cd200ecf6b475eb226a3
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 17:44:39 +0000

    feat: Display local time of post creation

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,11 +1,14 @@
 import { getPost } from "~/server/queries";
 import Link from "next/link";
+import { getLocalTime } from "~/app/_functions/functions";
 
 export default async function PostPage({ params }: { params: { id: string } }) {
   const postId = params.id;
   const post = await getPost(Number(postId));
   console.log(post);
   if (post) {
+    const time = getLocalTime(post.created_at);
+    console.log(time);
     return (
       <div className={"h-full w-full bg-white/10"}>
         <div>{post.content}</div>
