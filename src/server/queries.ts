f51b68bde8b1f7690775777d8139952151c90c18
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 16:58:34 +0000

    feat: Improve server-side post query functions

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -6,19 +6,12 @@ import { posts } from "~/server/db/schema";
 import { eq } from "drizzle-orm";
 
 export async function getPosts() {
-  console.log("getPosts (PLURAL)");
   return db.query.posts.findMany();
 }
 
 export async function getPost(id: number) {
-  console.log("getPost (NOT PLURAL)");
-  console.log("One");
   const user = auth();
-  console.log("Two");
-
   if (!user.userId) throw new Error("Unauthorized");
-  console.log("Three");
-
   return await db
     .select()
     .from(posts)
