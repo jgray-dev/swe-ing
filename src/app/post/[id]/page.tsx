9c266d6ffdf431340f043b0d3bbe9c4cda3576b7
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 20:38:03 +0000

    Refactor post page to use author_id and image_urls fields

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -9,17 +9,16 @@ export default function PostPage({ params }: { params: { id: string } }) {
 
   if (!post.isLoading) {
     if (post.data) {
-      console.log("IMAGES:");
-      console.log(post.data?.imageUrls);
       const time = new Date(post.data?.created_at).toLocaleString();
       return (
         <div className={"mx-auto my-4 ml-96 h-full"}>
           This is a post page for post id {postId}
           <div className={"pt-12"}>Time: {time}</div>
-          <div>Author: {post.data?.authorId}</div>
+          <div>Author: {post.data?.author_id}</div>
           <div>Content:{post.data?.content}</div>
-          {post.data?.imageUrls
-            ? post.data.imageUrls
+          {post.data?.image_urls
+            ? post.data.image_urls
+                // @ts-ignore
                 .split(",")
                 .map((url: string, index: number) => (
                   <img key={index} src={url.trim()} alt="image" width={400} />
