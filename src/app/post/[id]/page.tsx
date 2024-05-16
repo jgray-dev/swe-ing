f575f7dd3dc0c7838cd07d2f91d3ea5defeb4526
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 17:38:36 +0000

    feat(post/[id]/page): Handle post not found case and provide link to home

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getPost } from "~/server/queries";
+import Link from "next/link";
 
 export default async function PostPage({ params }: { params: { id: string } }) {
   const postId = params.id;
@@ -9,6 +10,19 @@ export default async function PostPage({ params }: { params: { id: string } }) {
       <div className={"h-full w-full bg-white/10"}>
         <div>{post.content}</div>
         This is a post page for post id {postId}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={
+          "fixed left-0 top-0 z-50 h-screen w-screen bg-black text-center text-2xl text-red-400"
+        }
+      >
+        <div>Post not found</div>
+        <div className="mt-4 text-lg text-white underline">
+          <Link href="/">Go back home</Link>
+        </div>
       </div>
     );
   }
