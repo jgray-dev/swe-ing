99f6946d365d97bb851fb493dab4e39145e39371
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 02:52:19 +0000

    Update queries.ts to add createPost function and handle unauthorized users.

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,8 +1,10 @@
 import "server-only";
+
 import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
 import { posts } from "~/server/db/schema";
 import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
 export async function getPosts() {
   return db.query.posts.findMany();
@@ -19,4 +21,16 @@ export async function getPost(id: number) {
     .where(eq(posts.id, id))
     .limit(1)
     .then((result) => result[0]);
+}
+
+export async function createPost(content: string, imageUrls: string[]) {
+  const user = auth();
+
+  if (!user.userId) throw new Error("Unauthorized");
+
+  await db.insert(posts).values({
+    authorId: user.userId,
+    content: content,
+    imageUrls: imageUrls,
+  });
 }
