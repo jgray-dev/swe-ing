95038438afa23190618c0e9c732dc6866d47609f
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-16 02:52:14 +0000

    Refactor post page to display post content and handle missing posts

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,11 +1,15 @@
 import { getPost } from "~/server/queries";
 
-export default async function PostPage({
-  params: { id: postId },
-}: {
-  params: { id: string };
-}) {
+export default async function PostPage({ params }: { params: { id: string } }) {
+  const postId = params.id;
   const post = await getPost(Number(postId));
   console.log(post);
-  return <div>This is a post page for post id {postId}</div>;
+  if (post) {
+    return (
+      <div className={"h-full w-full bg-white/10"}>
+        <div>{post.content}</div>
+        This is a post page for post id {postId}
+      </div>
+    );
+  }
 }
