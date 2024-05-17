4a48a2f2ddc82d3c362f4e565390f83c1f3dd202
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-17 19:22:07 +0000

    Reorder router imports in root.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -8,8 +8,8 @@ import {userRouter} from "~/server/api/routers/user";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
-  user: userRouter
+  user: userRouter,
+  post: postRouter
 });
 
 // export type definition of API
