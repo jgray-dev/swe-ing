20482ac29b33d23a410a4718ebf76b911f9ea75d
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-17 18:08:43 +0000

    okay so im a fucking idiot but its okay cuz i kinda understand it all now. trpc has an authedroute now, and everything *seems* to be working....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { api } from "~/trpc/server";
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
