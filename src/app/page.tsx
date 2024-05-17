fcbbe14c6d31b30cccfaf24a3dd0f96c8cf9556c
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-17 01:38:30 +0000

    feat(page): Display post author name and style post cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
 import Link from "next/link";
 import { getPosts } from "~/server/queries";
+import {getNameFromId} from "~/app/_functions/server";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const posts = await getPosts();
-
+  const posts = await getPosts(1);
   return (
-    <main className="h-screen w-screen bg-black/60 text-white">
-      <div className="flex flex-wrap gap-4">
-        {posts.map((post, index) => {
-          return (
-            <div key={index}>
-              <Link href={`/post/${post.id}`}>{post.content}</Link>
-            </div>
-          );
-        })}
+    <main className="h-screen w-screen bg-black/80 text-white">
+      <div className="flex w-screen items-center justify-center">
+        <div id="page" className={"md:px-4"}>
+          {posts.map(async (post) => {
+            return (
+              <Link href={`/post/${post.id}`}>
+                <div
+                  key={post.id}
+                  className={
+                    "my-2 h-fit rounded-md border mx-4 md:mx-0 border-white bg-black/70 py-4 backdrop-blur-sm md:w-96"
+                  }
+                >
+                  {post.content}
+                  {post.author_name}
+                </div>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </main>
   );
