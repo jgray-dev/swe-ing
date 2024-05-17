3cae69709be7124d1dfd068049d4547b5be50aa9
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-17 19:59:01 +0000

    Add /api/uploadthing to list of public routes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/api/webhook', '/sign-in', '/sign-up']);
+const isPublicRoute = createRouteMatcher(['/api/webhook', '/sign-in', '/sign-up', '/api/uploadthing']);
 
 export default clerkMiddleware((auth, request) => {
   if(!isPublicRoute(request)) {
