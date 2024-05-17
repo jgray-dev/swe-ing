4843fafc2fc950a0c1fe05ffb68e5a10b686e2ed
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-17 18:08:06 +0000

    feat(user-router): add hello, create, and getLatest mutations

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+import {authedProcedure, createTRPCRouter, publicProcedure} from "~/server/api/trpc";
+import { posts } from "~/server/db/schema";
+
+export const userRouter = createTRPCRouter({
+  hello: authedProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(posts).values({
+        name: input.name,
+      });
+    }),
+
+  getLatest: publicProcedure.query(({ ctx }) => {
+    return ctx.db.query.posts.findFirst({
+      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+    });
+  }),
+});
