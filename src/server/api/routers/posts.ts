a486c1ef33ff7667a6abdcc97c966728fe09685a
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 04:52:07 +0000

    Implement posts CRUD operations and add pagination to the home page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,23 +1,33 @@
 import { z } from "zod";
 
-import {authedProcedure, createTRPCRouter, publicProcedure} from "~/server/api/trpc";
-import {posts} from "~/server/db/schema";
-import {eq} from "drizzle-orm/sql/expressions/conditions";
-import {desc} from "drizzle-orm/sql/expressions/select";
+import {
+  authedProcedure,
+  createTRPCRouter,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { posts } from "~/server/db/schema";
+import { desc } from "drizzle-orm/sql/expressions/select";
 
 export const postsRouter = createTRPCRouter({
   create: authedProcedure
-    .input(z.object({ content: z.string().min(1),
-      imageUrls: z.array(z.string()).optional() }))
+    .input(
+      z.object({
+        content: z.string().min(1),
+        imageUrls: z.array(z.string()).optional(),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
-      return ctx.db.insert(posts).values({
-        author_id: `${ctx.fullUser.id}`,
-        author_name: `${ctx.fullUser.firstName} ${ctx.fullUser.lastName}`,
-        content: `${input.content}`,
-        image_urls: input.imageUrls,
-        created_at: Date.now(),
-        updated_at: Date.now(),
-      }).returning();
+      return ctx.db
+        .insert(posts)
+        .values({
+          author_id: `${ctx.fullUser.id}`,
+          author_name: `${ctx.fullUser.firstName} ${ctx.fullUser.lastName}`,
+          content: `${input.content}`,
+          image_urls: input.imageUrls,
+          created_at: Date.now(),
+          updated_at: Date.now(),
+        })
+        .returning();
     }),
   getHomePage: authedProcedure
     .input(z.object({ page: z.number().min(1).default(1) }))
@@ -26,15 +36,15 @@ export const postsRouter = createTRPCRouter({
       return ctx.db.query.posts.findMany({
         orderBy: [desc(posts.updated_at)],
         limit: 50,
-        offset: offset
-      })
+        offset: offset,
+      });
     }),
-  
+
   getSingle: publicProcedure
     .input(z.object({ id: z.number() }))
     .query(async ({ ctx, input }) => {
       return ctx.db.query.posts.findFirst({
         where: (post, { eq }) => eq(post.id, input.id),
-      })
+      });
     }),
 });
