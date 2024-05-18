e6b950e30ce2906ddf72d7dc28d39a00f84e958d
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 04:08:19 +0000

    Implement home page with API call and console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
-
-import { T3CREATEPOST } from "~/app/_components/T3-CREATE-POST";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  
+  const allPosts = api.posts.getHomePage({page: 1})
+  console.log(allPosts)
   return (
-    <main className="h-screen w-screen">
-      
+    <main className="h-screen w-screen pt-16 text-white">
+      home page gaga
     </main>
   );
 }
