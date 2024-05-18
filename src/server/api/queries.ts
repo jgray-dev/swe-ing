d00156d16291ee3a724192f4e0d040dcc02ffe99
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 19:41:53 +0000

    Increase page size to 5, add function to get user profile picture

--- src/server/api/queries.ts
+++ src/server/api/queries.ts
@@ -3,9 +3,10 @@
 import {db} from "~/server/db";
 import {posts} from "~/server/db/schema";
 import {desc} from "drizzle-orm/sql/expressions/select";
+import {clerkClient} from "@clerk/nextjs/server";
 
 export async function nextPostPage(page: number) {
-  const pageSize = 2;
+  const pageSize = 5;
   const offset = (page - 1) * pageSize;
   return db
     .select()
@@ -13,4 +14,18 @@ export async function nextPostPage(page: number) {
     .orderBy(desc(posts.updated_at))
     .offset(offset)
     .limit(pageSize);
+}
+
+
+export async function getProfilePic(userId: string) {
+  const profile = await clerkClient.users.getUser(userId)
+  if (profile) {
+    if (profile.hasImage) {
+      return profile.imageUrl
+    } else {
+      return null
+    }
+  } else {
+    return null
+  }
 }
