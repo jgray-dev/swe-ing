b5be995b093a347fdba9e7d317f6e9d29ef5581e
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 04:15:52 +0000

    Upgrade React import, add unstable_httpBatchStreamLink

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
-import { useState } from "react";
+import React, { useState } from "react";
 import SuperJSON from "superjson";
 
 import { type AppRouter } from "~/server/api/root";
