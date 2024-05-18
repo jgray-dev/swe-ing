2bdbcd15c6a357884eb04879f42ddfb15694ff6e
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 04:15:51 +0000

    Fetch home page data and remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const allPosts = api.posts.getHomePage({page: 1})
+  const allPosts = await api.posts.getHomePage({page: 1})
   console.log(allPosts)
   return (
     <main className="h-screen w-screen pt-16 text-white">
       home page gaga
+      
     </main>
   );
 }
