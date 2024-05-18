3526d8eab1292300a9d191cdf6b44294c0e34cdf
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 05:34:28 +0000

    done for tonight. gonna merge this all with main. i need to make the home page display the posts properly, and get replies working to start feeling like its coming together. I have the backend routes working correctly so far. user database and profiles will come later idk

--- src/app/_components/HomePage.tsx
+++ src/app/_components/HomePage.tsx
@@ -1,22 +1,13 @@
 "use client";
 
 import {useEffect, useState} from "react";
-import {api} from "~/trpc/react"
-
 export default function HomePage() {
-  // const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const [page, setPage] = useState(1)
   const [allPosts, setAllPosts] = useState([])
   
-  function getNextPage() {
-    const { data, isLoading, error } =  api.posts.getHomePage.useQuery({ page: 2 });
-    console.log("Page ", page, "content\n", data)
-  }
   
-  useEffect(()=>{
-    getNextPage()
-  },[])
   
   return (<div>hello from hom epage (client)
-  <button onClick={()=>getNextPage()}>load more</button></div>)
+  <button>load more</button></div>)
 }
