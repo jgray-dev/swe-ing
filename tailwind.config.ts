75743ed0494fd7c7220e355e566e67a4b70613f2
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 01:45:37 +0000

    Update Tailwind config with UploadThing integration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { withUt } from "uploadthing/tw";
 
-export default {
+export default withUt({
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
@@ -10,5 +10,5 @@ export default {
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+});
