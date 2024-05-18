8daa8b086c271c55a28334edadb1bf33acb5f132
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 05:33:15 +0000

    feat(HomePage): add pagination and fetch posts from API

--- src/app/_components/HomePage.tsx
+++ src/app/_components/HomePage.tsx
@@ -1,5 +1,22 @@
 "use client";
 
+import {useEffect, useState} from "react";
+import {api} from "~/trpc/react"
+
 export default function HomePage() {
-  return (<div>hello from hom epage (client)</div>)
+  // const [isLoading, setIsLoading] = useState(false)
+  const [page, setPage] = useState(1)
+  const [allPosts, setAllPosts] = useState([])
+  
+  function getNextPage() {
+    const { data, isLoading, error } =  api.posts.getHomePage.useQuery({ page: 2 });
+    console.log("Page ", page, "content\n", data)
+  }
+  
+  useEffect(()=>{
+    getNextPage()
+  },[])
+  
+  return (<div>hello from hom epage (client)
+  <button onClick={()=>getNextPage()}>load more</button></div>)
 }
