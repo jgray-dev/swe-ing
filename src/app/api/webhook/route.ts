1a4efa41aa46914dd85d9c54e7c2fd347a1aae91
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 22:41:39 +0000

    test build with webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,39 +1,36 @@
-// import type { NextApiRequest, NextApiResponse } from 'next';
-// import {NextResponse} from "next/server";
-// import { api } from "~/trpc/server";
-//
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//
-//   if (req.method === 'POST') {
-//     try {
-//       console.log('Received POST request');
-//       console.log('Headers:', req.headers);
-//       // Read the request body from the ReadableStream
-//       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
-//       const reader = req.body.getReader();
-//       const chunks = [];
-//       let totalLength = 0;
-//
-//       while (true) {
-//         // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
-//         const { done, value } = await reader.read();
-//         if (done) break;
-//         chunks.push(value);
-//         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-//         totalLength += value.length;
-//       }
-//       const bodyBuffer = Buffer.concat(chunks, totalLength);
-//       const bodyText = bodyBuffer.toString('utf-8');
-//       console.log(bodyText)
-//       return NextResponse.json({ error: 'Webhook received' }, { status: 200 })
-//     } catch (error) {
-//       console.error('Error processing webhook:', error);
-//       return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
-//     }
-//   } else {
-//     res.setHeader('Allow', 'POST');
-//     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
-//   }
-// }
-//
-// export { handler as POST };
+import type { NextApiRequest, NextApiResponse } from 'next';
+import {NextResponse} from "next/server";
+import {safeParseJSON} from "@uploadthing/shared";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  if (req.method === 'POST') {
+    try {
+      console.log('Received POST request');
+      console.log('Headers:', req.headers);
+      // Read the request body from the ReadableStream
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
+      const reader = req.body.getReader();
+      const chunks = [];
+
+      while (true) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
+        const { done, value } = await reader.read();
+        if (done) break;
+        chunks.push(value);
+      }
+      const bodyBuffer = Buffer.concat(chunks);
+      const bodyText = bodyBuffer.toString('utf-8');
+      console.log(safeParseJSON(bodyText))
+      return NextResponse.json({ error: 'Webhook received' }, { status: 200 })
+    } catch (error) {
+      console.error('Error processing webhook:', error);
+      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+  }
+}
+
+export { handler as POST };
