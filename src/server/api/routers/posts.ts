be5e6e934003dc5515603aa0ea0804ab2ef6b5cc
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 04:08:21 +0000

    Feat: Add home page and single post routes to posts router

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
-import {authedProcedure, createTRPCRouter} from "~/server/api/trpc";
-import { posts } from "~/server/db/schema";
+import {authedProcedure, createTRPCRouter, publicProcedure} from "~/server/api/trpc";
+import {posts} from "~/server/db/schema";
+import {eq} from "drizzle-orm/sql/expressions/conditions";
 
 export const postsRouter = createTRPCRouter({
   create: authedProcedure
@@ -14,6 +15,29 @@ export const postsRouter = createTRPCRouter({
         content: `${input.content}`,
         image_urls: input.imageUrls,
         created_at: Date.now(),
+        updated_at: Date.now(),
       }).returning();
+    }),
+  getHomePage: authedProcedure
+    .input(z.object({ page: z.number().min(1).default(1) }))
+    .query(async ({ ctx, input }) => {
+      const offset = (input.page - 1) * 25;
+      return ctx.db
+        .select()
+        .from(posts)
+        .orderBy(posts.updated_at)
+        .limit(25)
+        .offset(offset);
+    }),
+  
+  getSingle: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db
+        .select()
+        .from(posts)
+        .where(eq(posts.id, input.id))
+        .limit(1)
+        .then((result) => result[0]);
     }),
 });
