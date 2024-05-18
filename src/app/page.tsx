1d2827f79e9d47b8d01886230c7190b1ab6a400c
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 18:45:09 +0000

    Refactor HomePage component, remove unused tRPC import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { api } from "~/trpc/server";
+// import { api } from "~/trpc/server";
 import HomePage from "~/app/_components/HomePage";
 
 export default async function Home() {
-  const allPosts = await api.posts.getHomePage({ page: 1 });
-  console.log(allPosts);
   return (
     <div className="h-screen w-screen pt-16 text-white text-center">
       <HomePage />
