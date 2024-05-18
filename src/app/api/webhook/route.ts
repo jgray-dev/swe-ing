93cb95de07eff78073ba117c6b91a741502fa34f
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 22:50:22 +0000

    webhook route now gives us json.. im going to push often to test build bcuz last night it was failing because of this file, not sure why

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,18 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import {NextResponse} from "next/server";
-import {safeParseJSON} from "@uploadthing/shared";
+import { NextResponse } from "next/server";
+import { safeParseJSON } from "@uploadthing/shared";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
   if (req.method === 'POST') {
     try {
-      console.log('Received POST request');
-      console.log('Headers:', req.headers);
-      // Read the request body from the ReadableStream
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
       const reader = req.body.getReader();
       const chunks = [];
-
       while (true) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
         const { done, value } = await reader.read();
@@ -21,15 +16,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
       const bodyBuffer = Buffer.concat(chunks);
       const bodyText = bodyBuffer.toString('utf-8');
-      console.log(safeParseJSON(bodyText))
-      return NextResponse.json({ error: 'Webhook received' }, { status: 200 })
+      const jsonBody = await safeParseJSON(bodyText);
+      console.log('Request Body:', jsonBody);
+
+
+      return NextResponse.json({ message: 'Webhook received' }, { status: 200 });
     } catch (error) {
       console.error('Error processing webhook:', error);
-      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
   } else {
     res.setHeader('Allow', 'POST');
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    return NextResponse.json({ error: 'Method Not Allowed' }, { status: 405 });
   }
 }
 
