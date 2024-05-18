28aaa928b3f6e48ce05e6e2e7451b29c22c8eb4b
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 18:45:09 +0000

    Implement NextJS API route for fetching paginated posts

---
+++ src/server/api/queries.ts
@@ -0,0 +1,17 @@
+"use server"
+
+import {db} from "~/server/db";
+import {posts} from "~/server/db/schema";
+import {desc} from "drizzle-orm/sql/expressions/select";
+
+
+export async function nextPostPage(page: number) {
+  const pageSize = 2;
+  const offset = (page - 1) * pageSize;
+  return db
+    .select()
+    .from(posts)
+    .orderBy(desc(posts.updated_at))
+    .offset(offset)
+    .limit(pageSize);
+}
