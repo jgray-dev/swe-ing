c44ba03322ab372beff8292c61d66cb389a43175
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-18 23:19:55 +0000

    Update post author info when profile is updated, created, or deleted

--- src/server/api/queries.ts
+++ src/server/api/queries.ts
@@ -3,7 +3,7 @@
 import {db} from "~/server/db";
 import {posts} from "~/server/db/schema";
 import {desc} from "drizzle-orm/sql/expressions/select";
-import {clerkClient} from "@clerk/nextjs/server";
+import {eq} from "drizzle-orm/sql/expressions/conditions";
 
 export async function nextPostPage(page: number) {
   const pageSize = 5;
@@ -16,16 +16,36 @@ export async function nextPostPage(page: number) {
     .limit(pageSize);
 }
 
-
-export async function getProfilePic(userId: string) {
-  const profile = await clerkClient.users.getUser(userId)
-  if (profile) {
-    if (profile.hasImage) {
-      return profile.imageUrl
-    } else {
-      return null
-    }
-  } else {
-    return null
+export interface profile {
+  data: {
+    image_url: string,
+    id: string,
+    first_name: string | null,
+    last_name: string | null,
   }
+  type: string,
+}
+
+
+async function updatePosts(profile: profile) {
+  console.log("UPDATE USER PROFILE:")
+  console.log(profile)
+  return db.update(posts)
+    .set({ author_url: `${profile.data.image_url}`, author_name: `${profile.data.first_name ? profile.data.first_name : "Unknown"} ${profile.data.last_name ? profile.data.last_name : ""} ` })
+    .where(eq(posts.author_id, profile.data.id))
+    .returning();
+}
+
+
+export async function updateProfile(profile: profile) {
+  console.log("update profile: ", profile.type)
+  void updatePosts(profile)
+}
+
+
+export async function createProfile(profile: profile) {
+  console.log("create profile: ", profile.type)
+}
+export async function deleteProfile(profile: profile) {
+  console.log("delete profile: ", profile.type)
 }
