921ab378537e87a76117315f267b9376d61ed99f
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-20 07:14:07 +0000

    Add Clerk.js authentication to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import HomePage from "~/app/_components/HomePage";
+import {RedirectToSignIn, SignedIn, SignedOut} from "@clerk/nextjs";
 
 export default async function Home() {
   return (
     <div className="h-screen w-screen pt-16 text-center text-white">
-      <HomePage />
+      <SignedIn >
+        <HomePage />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </div>
   );
 }
