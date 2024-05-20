cb599d1e66af55d069481a7aa846ee010fc4d858
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-20 06:31:23 +0000

    Refactor interfaces and add author details in post interface

--- src/app/_components/interfaces.ts
+++ src/app/_components/interfaces.ts
@@ -8,14 +8,22 @@ export interface profile {
   type: string;
 }
 
-export interface Post {
+export interface post {
   id: number;
-  author_id: string;
-  author_name: string;
-  author_url: string;
+  author_id: number;
   content: string;
   image_urls: string[] | null;
   post_tags: string;
   created_at: number;
   updated_at: number;
+  author?: {
+    id: number;
+    clerk_id: string;
+    name: string;
+    image_url: string;
+    bio: string;
+    location: string;
+    website: string;
+    skills: string;
+  };
 }
