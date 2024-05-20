e92b314a57e03246a9979afa83150cf6a53b7923
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-20 06:31:26 +0000

    Rename userRouter to usersRouter and update appRouter accordingly

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
-import { userRouter } from "~/server/api/routers/user";
+import { usersRouter } from "~/server/api/routers/user";
 import { postsRouter } from "~/server/api/routers/posts";
 
 /**
@@ -8,7 +8,7 @@ import { postsRouter } from "~/server/api/routers/posts";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  user: userRouter,
+  users: usersRouter,
   posts: postsRouter,
 });
 
