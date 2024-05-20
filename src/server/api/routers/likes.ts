253f87bb0fd7a597e1c0965fcf58841bfcb5f798
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-20 21:32:56 +0000

    Refactor like logic and add comments for clarity

--- src/server/api/routers/likes.ts
+++ src/server/api/routers/likes.ts
@@ -1,7 +1,7 @@
-import {authedProcedure, createTRPCRouter} from "~/server/api/trpc";
-import {z} from "zod";
-import {likes} from "~/server/db/schema";
-import {and, eq} from "drizzle-orm/sql/expressions/conditions";
+import { authedProcedure, createTRPCRouter } from "~/server/api/trpc";
+import { z } from "zod";
+import { likes } from "~/server/db/schema";
+import { and, eq } from "drizzle-orm/sql/expressions/conditions";
 
 export const likesRouter = createTRPCRouter({
   create: authedProcedure
@@ -14,22 +14,27 @@ export const likesRouter = createTRPCRouter({
       const user = await ctx.db.query.users.findFirst({
         where: (user, { eq }) => eq(user.clerk_id, ctx.fullUser.id),
       });
-      console.log(ctx.fullUser.id)
+      console.log(ctx.fullUser.id);
       if (user) {
         const previousLike = await ctx.db.query.likes.findFirst({
-          where: and(eq(likes.post_id, input.post_id),eq(likes.user_id, user.id))
+          where: and(
+            eq(likes.post_id, input.post_id),
+            eq(likes.user_id, user.id),
+          ),
         });
         if (previousLike) {
-          await ctx.db.delete(likes).where(and(eq(likes.post_id, input.post_id),eq(likes.user_id, user.id)));
-          return "Unliked"
+          await ctx.db
+            .delete(likes)
+            .where(
+              and(eq(likes.post_id, input.post_id), eq(likes.user_id, user.id)),
+            );
+          return "Unliked";
         }
-        await ctx.db
-          .insert(likes)
-          .values({
-            user_id: user.id,
-            post_id: input.post_id,
-          })
-        return "Liked"
+        await ctx.db.insert(likes).values({
+          user_id: user.id,
+          post_id: input.post_id,
+        });
+        return "Liked";
       } else {
         return "NO USER";
       }
