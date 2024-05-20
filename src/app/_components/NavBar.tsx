c5ca713cf6d92148943fa1a15e25ba89ce22fdd7
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-20 06:31:22 +0000

    Refactor NavBar component to use RedirectToSignIn and improve styling

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,40 +1,36 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import Link from "next/link";
-import { FaPlusSquare, FaRegUserCircle } from "react-icons/fa";
+import { FaPlusSquare } from "react-icons/fa";
 import { IoHome } from "react-icons/io5";
 
 export default function NavBar() {
   return (
     <div
       className={
-        "fixed top-0 z-50 flex h-16 max-h-16 w-full items-center justify-between bg-black/50 p-4 px-8 text-lg font-semibold text-white backdrop-blur-md border-b border-white/50"
+        "fixed top-0 z-50 flex h-16 max-h-16 w-full items-center justify-between border-b border-white/50 bg-black/50 p-4 px-8 text-lg font-semibold text-white backdrop-blur-md"
       }
     >
       <Link href={"/"}>
         <IoHome className="h-7 w-7 cursor-pointer fill-zinc-400 duration-200 hover:fill-white" />
       </Link>
-      <SignedIn>
-        <div>
-          <Link href={`/newpost`}>
-            <FaPlusSquare className="h-7 w-7 cursor-pointer fill-zinc-400 duration-200 hover:fill-white" />
-          </Link>
-        </div>
-        <div>
+      <div>
+        <Link href={`/newpost`}>
+          <FaPlusSquare className="h-7 w-7 cursor-pointer fill-zinc-400 duration-200 hover:fill-white" />
+        </Link>
+      </div>
+      <div>
+        <SignedIn>
           <UserButton />
-        </div>
-      </SignedIn>
-      <SignedOut>
-        <SignInButton>
-          <div className="group flex flex-row pl-4 text-zinc-400 duration-200 hover:text-white">
-            <span>Sign in</span>
-            <FaRegUserCircle
-              className={
-                "ml-2 h-7 w-7 cursor-pointer fill-zinc-400 duration-200 group-hover:fill-white"
-              }
-            />
-          </div>
-        </SignInButton>
-      </SignedOut>
+        </SignedIn>
+        <SignedOut>
+          <RedirectToSignIn />
+        </SignedOut>
+      </div>
     </div>
   );
 }
