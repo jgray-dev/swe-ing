3987a4fc50fce1a05a393b20e1571959c00817ab
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-20 07:26:36 +0000

    feat: Integrate Clerk.js and implement sign-in/sign-out flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import HomePage from "~/app/_components/HomePage";
-import {RedirectToSignIn, SignedIn, SignedOut} from "@clerk/nextjs";
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default async function Home() {
   return (
     <div className="h-screen w-screen pt-16 text-center text-white">
-      <SignedIn >
+      <SignedIn>
         <HomePage />
       </SignedIn>
       <SignedOut>
