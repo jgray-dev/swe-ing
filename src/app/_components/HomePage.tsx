597903ae03de2449c9d294c06cde23c8332de5fe
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-20 20:04:39 +0000

    feat(LikeButton): Implement like button functionality with state and API call

--- src/app/_components/HomePage.tsx
+++ src/app/_components/HomePage.tsx
@@ -2,36 +2,50 @@
 
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { nextPostPage } from "~/server/api/queries";
+import { getDbUser, nextPostPage } from "~/server/api/queries";
 import Link from "next/link";
-import type { post } from "~/app/_components/interfaces";
+import type { like, post } from "~/app/_components/interfaces";
 import { useUser } from "@clerk/shared/react";
 import { CiHeart, CiShare1 } from "react-icons/ci";
 import { GoCommentDiscussion } from "react-icons/go";
 import { PiDotsNine } from "react-icons/pi";
-import PostContextMenu from "~/app/_components/PostContextMenu";
+// import PostContextMenu from "~/app/_components/PostContextMenu";
+import { api } from "~/trpc/react";
+import LikeButton from "~/app/_components/LikeButton";
 
 export default function HomePage() {
   const [loading, setLoading] = useState(false);
+  const [end, setEnd] = useState(false);
   const [page, setPage] = useState(1);
   const [allPosts, setAllPosts] = useState<Array<post>>([]);
   const [cards, setCards] = useState<React.ReactElement[]>([]);
-  const [userId, setUserId] = useState<string | null>(null);
-  const [contextMenu, setContextMenu] = useState(<></>);
+  const [likedPosts, setLikedPosts] = useState<number[]>([]);
+  const [userId, setUserId] = useState<number | undefined>(undefined);
+  const { user, isSignedIn } = useUser();
 
-  const { isSignedIn, user } = useUser();
+  async function dbUser(clerkId: string) {
+    return getDbUser(clerkId);
+  }
   useEffect(() => {
-    if (isSignedIn) {
-      setUserId(user.id);
-    }
+    void firstLoad();
     //eslint-disable-next-line
   }, [isSignedIn]);
 
-  useEffect(() => {
-    void fetchData();
-    setLoading(true);
-    //eslint-disable-next-line
-  }, []);
+  async function firstLoad() {
+    if (!userId) {
+      if (isSignedIn) {
+        await dbUser(user.id).then(async (data) => {
+          if (data) {
+            setUserId(data.id);
+            setLoading(true);
+            void (await fetchData(data.id));
+          } else {
+            console.error("Error fetching user from local database");
+          }
+        });
+      }
+    }
+  }
 
   useEffect(() => {
     const handleScroll = () => {
@@ -39,191 +53,199 @@ export default function HomePage() {
       const scrollTop = div ? div.scrollTop : 0;
       const scrollHeight = div ? div.scrollHeight : 0;
       const clientHeight = div ? div.clientHeight : 0;
-      if (scrollTop + clientHeight >= scrollHeight - 250 && !loading) {
-        setLoading(true);
-        void fetchData();
+      if (scrollTop + clientHeight >= scrollHeight - 750 && !loading) {
+        console.log("Loading more posts");
+        void fetchData(userId);
         setPage((prevPage) => prevPage + 1);
       }
     };
     const div = document.getElementById("scrolls");
     div?.addEventListener("scroll", handleScroll);
+    if (end) {
+      div?.removeEventListener("scroll", handleScroll);
+    }
     return () => {
       div?.removeEventListener("scroll", handleScroll);
     };
     //eslint-disable-next-line
   }, [loading]);
 
-  async function fetchData() {
-    if (!loading) {
-      const data = await nextPostPage(page);
-      if (!data) {
-        console.warn("No data returned from server");
-        return;
-      }
-      if (data.length > 0) {
-        const newPosts = data.filter(
-          (newPost) => !allPosts.some((post) => post.id === newPost.id),
-        );
-        if (newPosts.length > 0) {
-          console.log("new posts", newPosts);
-          setAllPosts((prevPosts) => [...prevPosts, ...newPosts]);
-          setCards([...cards, ...getCards(newPosts)]);
-        }
+  async function fetchData(user_id?: number) {
+    setLoading(true);
+    const data = await nextPostPage(page);
+    if (!data) {
+      console.warn("No data returned from server");
+      return;
+    }
+    if (data.length > 0) {
+      const newPosts = data.filter(
+        (newPost) => !allPosts.some((post) => post.id === newPost.id),
+      );
+      if (newPosts.length > 0) {
+        const newLikedPosts = [...likedPosts];
+        newPosts.forEach((post: post) => {
+          if (post.likes) {
+            post.likes.forEach((like: like) => {
+              if (like.user_id === user_id) {
+                newLikedPosts.push(post.id);
+              }
+            });
+          }
+        });
+        setLikedPosts(newLikedPosts);
+        setAllPosts([...allPosts, ...newPosts]);
+        setCards([...cards, ...getCards(newPosts, user_id)]);
         setLoading(false);
-      } else {
-        console.warn("End of posts");
       }
     } else {
-      console.warn("User scrolled, but we're already fetching more data");
+      console.warn("End of posts");
+      setEnd(true);
+    }
+    if (!end) {
+      setLoading(false);
     }
   }
 
-  function likePost(id: number) {
-    console.log("like post ", id);
-  }
+  
+
+  
+
   function showContextMenu(id: number) {
-    setContextMenu(<PostContextMenu />);
     console.log("context menu for post ", id);
   }
   async function sharePost(id: number, title: string) {
-    console.log("share post ", id);
-    await navigator.share({
+    const share = {
       url: `https://swe.ing/post/${id}`,
       title: `${title}`,
-    });
+    };
+    if (!navigator.canShare) {
+      alert("Your environment does not support sharing");
+    } else {
+      await navigator.share(share);
+    }
   }
 
-  function getCards(data: post[]): React.ReactElement[] {
-    return data.map((post) => {
-      return (
-        <div
-          key={post.created_at + post.id + Math.random()}
-          className={
-            "backdrop-blur-xs my-2 min-h-fit w-[99%] translate-x-[0.5%] rounded-lg border border-white/50 bg-black/90 p-1.5 text-zinc-200 duration-300"
-          }
-        >
-          <div className={"flex flex-col"}>
-            <div className={"flex h-full min-h-36 w-full flex-row"}>
-              <div className={"flex flex-col"}>
-                <div
-                  className={
-                    "flex w-20 min-w-20 max-w-20 flex-col items-center border-r border-white/50 pr-2 text-xs"
-                  }
-                >
-                  <div className="relative h-12 w-12 overflow-hidden rounded-full">
-                    <Link href={`/user/${post.author_id}`}>
-                      <Image
-                        // @ts-expect-error fuck typescript
-                        src={post.author.image_url}
-                        fill
-                        loading={"lazy"}
-                        className="object-cover"
-                        alt=""
-                        sizes="40px"
-                      />
-                    </Link>
-                  </div>
-                  {/*@ts-expect-error fuck typescript*/}
-                  {post.author.name}
-                </div>
-                <div
-                  className={
-                    "mr-1 h-fit min-h-0 w-20 min-w-20 max-w-20 border-r border-t border-white/50"
-                  }
-                >
-                  <div className={"flex max-h-24 flex-wrap overflow-y-hidden"}>
-                    {post.post_tags
-                      ? post.post_tags.split(",").map((tag) => {
-                          if (tag !== "") {
-                            return (
-                              <Link key={Math.random()} href={`/search/${tag}`}>
-                                <div
-                                  key={Math.random()}
-                                  className="mx-0.5 ml-0 mt-1 w-fit max-w-20 overflow-x-hidden truncate rounded-sm bg-white/5 p-0.5 text-left text-xs text-zinc-500"
-                                  title={tag}
-                                >
-                                  {tag}
-                                </div>
-                              </Link>
-                            );
-                          } else {
-                            return null;
-                          }
-                        })
-                      : null}
-                  </div>
-                </div>
-              </div>
-              <Link
-                key={post.created_at + post.id + Math.random()}
-                href={`/post/${post.id}`}
-              >
-                <div
-                  className={
-                    "line-clamp-[10] h-fit max-h-72 min-h-36 min-w-full max-w-full text-wrap pl-2 text-left"
-                  }
-                >
-                  {post.content}
-                </div>
-              </Link>
-            </div>
-            <div className={"mt-2 border-t border-white/50"}>
-              <div className={"flex flex-row justify-between px-4 pt-1.5"}>
-                <div
-                  className={"group flex flex-row text-zinc-400"}
-                  onClick={() => likePost(post.id)}
-                >
-                  <CiHeart
-                    className={
-                      "mr-1.5 h-6 w-6 duration-150 group-hover:text-white"
-                    }
-                  />
-                  <span
-                    className={
-                      "invisible absolute inline-flex h-6 w-6 rounded-full bg-red-400/30 group-hover:visible group-hover:animate-ping"
-                    }
-                  ></span>
-                  <span className={"duration-150 group-hover:text-white"}>
-                    {post.likes ? post.likes.length : 0}
-                  </span>
-                </div>
+  function getCards(data: post[], user_id?: number): React.ReactElement[] {
+    return data.map((post) => getCard(post, user_id));
+  }
 
-                <div className={"group flex flex-row text-zinc-400"}>
-                  <Link href={`/post/${post.id}`}>
-                    <GoCommentDiscussion
-                      className={
-                        "mr-1.5 h-6 w-6 duration-150 group-hover:text-white motion-safe:group-hover:-translate-y-[5%] motion-safe:group-hover:rotate-3"
-                      }
+  function getCard(post: post, user_id?: number): React.ReactElement {
+    if (userId) {
+      user_id = userId;
+    }
+    const liked = post.likes?.some((like) => like.user_id === user_id) ?? false;
+    return (
+      <div
+        key={post.created_at + post.id + Math.random()}
+        className={
+          "backdrop-blur-xs my-2 min-h-fit w-[99%] translate-x-[0.5%] rounded-lg border border-white/50 bg-black/90 p-1.5 text-zinc-200 duration-300"
+        }
+      >
+        <div className={"flex flex-col"}>
+          <div className={"flex h-full min-h-36 w-full flex-row"}>
+            <div className={"flex flex-col"}>
+              <div
+                className={
+                  "flex w-20 min-w-20 max-w-20 flex-col items-center border-r border-white/50 pr-2 text-xs"
+                }
+              >
+                <div className="relative h-12 w-12 overflow-hidden rounded-full">
+                  <Link href={`/user/${post.author_id}`}>
+                    <Image
+                      // @ts-expect-error fuck typescript
+                      src={post.author.image_url}
+                      fill
+                      loading={"lazy"}
+                      className="object-cover"
+                      alt=""
+                      sizes="40px"
                     />
                   </Link>
-                  <span className={"duration-150 group-hover:text-white"}>
-                    {post.comments ? post.comments.length : 0}
-                  </span>
                 </div>
+                {/*@ts-expect-error fuck typescript*/}
+                {post.author.name}
+              </div>
+              <div
+                className={
+                  "mr-1 h-fit min-h-0 w-20 min-w-20 max-w-20 border-r border-t border-white/50"
+                }
+              >
+                <div className={"flex max-h-24 flex-wrap overflow-y-hidden"}>
+                  {post.post_tags
+                    ? post.post_tags.split(",").map((tag) => {
+                        if (tag !== "") {
+                          return (
+                            <Link key={Math.random()} href={`/search/${tag}`}>
+                              <div
+                                key={Math.random()}
+                                className="mx-0.5 ml-0 mt-1 w-fit max-w-20 overflow-x-hidden truncate rounded-sm bg-white/5 p-0.5 text-left text-xs text-zinc-500"
+                                title={tag}
+                              >
+                                {tag}
+                              </div>
+                            </Link>
+                          );
+                        } else {
+                          return null;
+                        }
+                      })
+                    : null}
+                </div>
+              </div>
+            </div>
+            <Link
+              key={post.created_at + post.id + Math.random()}
+              href={`/post/${post.id}`}
+            >
+              <div
+                className={
+                  "line-clamp-[10] h-fit max-h-72 min-h-36 min-w-full max-w-full text-wrap pl-2 text-left"
+                }
+              >
+                {post.content}
+              </div>
+            </Link>
+          </div>
+          <div className={"mt-2 border-t border-white/50"}>
+            <div className={"flex flex-row justify-between px-4 pt-1.5"}>
+              
+              <LikeButton postId={Number(post.id)} dbliked={liked} dblikes={post.likes?post.likes.length:0}/>
 
-                <div>
-                  <CiShare1
+              <div className={"group flex flex-row text-zinc-400"}>
+                <Link href={`/post/${post.id}`}>
+                  <GoCommentDiscussion
                     className={
-                      "h-6 w-6 text-zinc-400 duration-150 hover:text-white motion-safe:hover:-translate-y-0.5 motion-safe:hover:translate-x-0.5"
+                      "mr-1.5 h-6 w-6 duration-150 group-hover:text-white motion-safe:group-hover:-translate-y-[5%] motion-safe:group-hover:rotate-3"
                     }
-                    onClick={() => sharePost(post.id, post.content)}
                   />
-                </div>
+                </Link>
+                <span className={"duration-150 group-hover:text-white"}>
+                  {post.comments ? post.comments.length : 0}
+                </span>
+              </div>
 
-                <div>
-                  <PiDotsNine
-                    className={
-                      "h-6 w-6 text-zinc-400 duration-150 hover:text-white motion-safe:hover:scale-110"
-                    }
-                    onClick={() => showContextMenu(post.id)}
-                  />
-                </div>
+              <div>
+                <CiShare1
+                  className={
+                    "h-6 w-6 text-zinc-400 duration-150 hover:text-white motion-safe:hover:-translate-y-0.5 motion-safe:hover:translate-x-0.5"
+                  }
+                  onClick={() => sharePost(post.id, post.content)}
+                />
+              </div>
+
+              <div>
+                <PiDotsNine
+                  className={
+                    "h-6 w-6 text-zinc-400 duration-150 hover:text-white motion-safe:hover:scale-110"
+                  }
+                  onClick={() => showContextMenu(post.id)}
+                />
               </div>
             </div>
           </div>
         </div>
-      );
-    });
+      </div>
+    );
   }
 
   return (
@@ -235,7 +257,7 @@ export default function HomePage() {
         <div className={"overflow-y-scroll"}>
           {cards}
           <div className={"pb-12 pt-24"}>
-            The end. <br />
+            The end. <br/>
             <Link href={"/newpost"} className={"underline"}>
               {" "}
               How about creating a new post
---
+++ src/app/_components/LikeButton.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React, {useState} from "react";
+import {CiHeart} from "react-icons/ci";
+import {api} from "~/trpc/react";
+
+interface LikeButtonProps {
+  postId: number;
+  dbliked: boolean;
+  dblikes: number;
+}
+
+export default function LikeButton({ postId, dbliked, dblikes }: LikeButtonProps): React.ReactElement {
+  const [liked, setLiked] = useState(dbliked);
+  const [likes, setLikes] = useState(dblikes);
+
+  const createPostLike = api.likes.create.useMutation({
+    onSuccess: (data) => {
+      if (!data) {
+        alert("Erorr creating post");
+        return;
+      }
+    },
+    onError: (err) => {
+      console.error(err.message);
+    },
+  });
+  
+  async function likePost() {
+    if (liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+    void createPostLike.mutateAsync({ post_id: postId });
+  }
+  return (
+    <div
+      className={"group flex flex-row text-zinc-400 w-12 min-w-12 max-w-12"}
+      onClick={() => likePost()}
+    >
+      <CiHeart
+        className={`mr-1.5 h-6 w-6 duration-150 ${liked ? "text-red-500 " : "group-hover:text-white"}`}
+      />
+      <span
+        className={`invisible absolute inline-flex h-6 w-6 rounded-full ${liked ? "bg-transparent" : "bg-red-400/40"} group-hover:visible group-hover:animate-ping`}
+      ></span>
+      <span className={"duration-150 group-hover:text-white"}>
+      {likes}
+      </span>
+    </div>
+  )
+}
+  
