51b527b5df31261bc3c58e69fdba423f01e1615f
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-21 22:19:05 +0000

    Update NewPost component styles to reorder backdrop-blur-sm class

--- src/app/newpost/page.tsx
+++ src/app/newpost/page.tsx
@@ -6,7 +6,7 @@ export default async function NewPost() {
   return (
     <div
       className={
-        "backdrop-blur-sm h-screen w-screen mx-auto bg-black/50 pt-16 text-center text-white"
+        "mx-auto h-screen w-screen bg-black/50 pt-16 text-center text-white backdrop-blur-sm"
       }
     >
       <ClientSide />
