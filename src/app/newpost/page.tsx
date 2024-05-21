c662910c474abd72481ed8cddfa69e0e705a5b7b
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-21 19:01:54 +0000

    Update NewPost component styles to use backdrop-blur-sm and adjust styles

--- src/app/newpost/page.tsx
+++ src/app/newpost/page.tsx
@@ -6,7 +6,7 @@ export default async function NewPost() {
   return (
     <div
       className={
-        "backdrop-blur-xs mx-auto h-screen w-screen bg-black/50 pt-16 text-center text-white"
+        "backdrop-blur-sm h-screen w-screen mx-auto bg-black/50 pt-16 text-center text-white"
       }
     >
       <ClientSide />
