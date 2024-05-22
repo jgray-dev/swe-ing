3ab94433bd34dd80b263fc4208d17df28cf6acd8
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-22 02:23:40 +0000

    Update likes router to handle liking/unliking posts and manage recent likes

--- src/server/api/routers/likes.ts
+++ src/server/api/routers/likes.ts
@@ -2,7 +2,7 @@ import { authedProcedure, createTRPCRouter } from "~/server/api/trpc";
 import { z } from "zod";
 import {likes, users} from "~/server/db/schema";
 import { and, eq } from "drizzle-orm/sql/expressions/conditions";
-import {getAverageEmbedding, getPostEmbeddings, getUserEmbedding} from "~/app/_components/embedding";
+import {getAverageEmbedding, getPostEmbeddings} from "~/app/_components/embedding";
 
 export const likesRouter = createTRPCRouter({
   create: authedProcedure
@@ -24,47 +24,32 @@ export const likesRouter = createTRPCRouter({
           ),
         });
 
-        if (previousLike) {
-          // Unlike the post
+        if (previousLike) { // Unlike the post
           const newLikes = (user.recent_likes ?? []).filter(id => id !== input.post_id);
-
-          await ctx.db.update(users)
-            .set({
-              recent_likes: newLikes,
-            })
-            .where(eq(users.id, user.id));
-
           await ctx.db
             .delete(likes)
             .where(
               and(eq(likes.post_id, input.post_id), eq(likes.user_id, user.id)),
             );
-          // void getUserEmbedding(user as user)
-          const embeddings = getPostEmbeddings(newLikes)
+          await ctx.db.update(users)
+            .set({
+              recent_likes: newLikes
+            })
+            .where(eq(users.id, user.id));
           return "Unliked";
         }
-
-        // Add the new like to the recent_likes array
+        // Like the post
         const newLikes = [input.post_id, ...(user.recent_likes ?? [])];
-
-        // Keep only the most recent 50 likes
-        const recentLikes = newLikes.slice(0, 5);
-
-
+        const recentLikes = newLikes.slice(0, 20);
         await ctx.db.insert(likes).values({
           user_id: user.id,
           post_id: input.post_id,
         });
-        const embeddings = await getPostEmbeddings(recentLikes)
-        const userEmbedding = await getAverageEmbedding(embeddings)
-
         await ctx.db.update(users)
           .set({
             recent_likes: recentLikes,
-            embedding: userEmbedding
           })
           .where(eq(users.id, user.id));
-        // void getUserEmbedding(user as user)
         return "Liked";
       } else {
         return "NO USER";
