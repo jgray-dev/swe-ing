72dc02fe0f7689d3a687b36b78b630c22dd9f25f
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-22 01:37:35 +0000

    Implement functionalities for getting vector embeddings and average embeddings

--- src/app/_components/embedding.ts
+++ src/app/_components/embedding.ts
@@ -2,13 +2,62 @@
 
 import { embed } from "ai";
 import { openai } from "@ai-sdk/openai";
-import { db } from "~/server/db";
-import { posts } from "~/server/db/schema";
+import type {user} from "~/app/_components/interfaces"
+import {db} from "~/server/db";
+import {inArray} from "drizzle-orm/sql/expressions/conditions";
 
-export async function getEmbedding(text: string) {
+export async function getEmbedding(text: string, tags?: string) {
   const { embedding } = await embed({
     model: openai.embedding("text-embedding-3-small"),
-    value: text,
+    value: text + tags,
   });
   return embedding;
+}
+
+
+export async function getAverageEmbedding(embeddings: number[][]) {
+  const average: number[] = [];
+  const temp:number[] = []
+  for (const embed of embeddings) {
+    // @ts-expect-error fuck typescript
+    temp.push(embed[0])
+  }
+  console.log(temp)
+  for (let i = 0; i < 1536; i++) {
+    let sum = 0;
+    let count = 0;
+
+    for (const item of embeddings) {
+      if (item[i] !== undefined) {
+        // @ts-expect-error fuck typescript
+        sum += item[i];
+        count++;
+      }
+    }
+    average[i] = count > 0 ? sum / count : 0;
+  }
+  console.log(average[0])
+  return average;
+}
+
+
+export async function getUserEmbedding(user: user) {
+  console.log(user.recent_likes)
+}
+
+
+export async function getPostEmbeddings(postIds: number[]) {
+  if (postIds.length === 0) {
+    return [];
+  }
+  const allPosts = await db.query.posts.findMany({
+    where: (post) => inArray(post.id, postIds),
+    columns: {
+      embedding: true,
+    },
+  });
+  const returnList:number[][] = []
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  allPosts.forEach((post)=>returnList.push(post.embedding))
+  return returnList
 }
