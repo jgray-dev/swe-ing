c03a188dc2fe59c09c2daf91044bcc7cd4f3c1f5
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-23 18:56:27 +0000

    Update bun.lockb with generated lock file changes

--- src/app/_functions/embedding.ts
+++ src/app/_functions/embedding.ts
@@ -4,6 +4,7 @@ import { embed } from "ai";
 import { openai } from "@ai-sdk/openai";
 import { db } from "~/server/db";
 import { inArray } from "drizzle-orm/sql/expressions/conditions";
+import {embeddingFromID} from "~/server/api/server-only";
 
 export async function getEmbedding(text: string, tags?: string) {
   const { embedding } = await embed({
@@ -18,7 +19,7 @@ export async function getAverageEmbedding(embeddings: number[][]) {
   for (let i = 0; i < 1536; i++) {
     let sum = 0;
     let count = 0;
-
+    
     for (const item of embeddings) {
       if (item[i] !== undefined) {
         // @ts-expect-error fuck typescript
@@ -31,18 +32,34 @@ export async function getAverageEmbedding(embeddings: number[][]) {
   return average;
 }
 
+//Take an array of Post ID's, and return an array of the embeddings (number[][])
 export async function getPostEmbeddings(postIds: number[]) {
+  console.log("getPostEmbeddings(", postIds, ")")
   if (postIds.length === 0) {
     return [];
   }
-  const allPosts = await db.query.posts.findMany({
-    where: (post) => inArray(post.id, postIds),
-    columns: {
-      embedding: true,
-    },
-  });
-  const returnList: number[][] = [];
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  allPosts.forEach((post) => returnList.push(post.embedding));
-  return returnList;
+  
+  const allEmbeds = []
+  for (const id of postIds) {
+    const idValue = await embeddingFromID("posts", id);
+    if (idValue) {
+      allEmbeds.push(idValue)
+    }
+  }
+  return allEmbeds
+  
+  
+  
+  
+  // Pre-PC code
+  // const allPosts = await db.query.posts.findMany({
+  //   where: (post) => inArray(post.id, postIds),
+  //   columns: {
+  //     embedding: true,
+  //   },
+  // });
+  // const returnList: number[][] = [];
+  // // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  // allPosts.forEach((post) => returnList.push(post.embedding));
+  // return returnList;
 }
