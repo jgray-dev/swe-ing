5ac4484abc65823526c36e773dcb85583deed198
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-28 04:24:51 +0000

    Add reply functionality to post page using useReplyState

--- src/app/post/[id]/_NewReply.tsx
+++ src/app/post/[id]/_NewReply.tsx
@@ -1,12 +1,13 @@
-import {useUserState} from "~/app/_functions/store";
+import {useReplyState, useUserState} from "~/app/_functions/store";
 
-export default function NewReply(post_id: number) {
+export default function NewReply() {
+  const {post_id} = useReplyState(state => state)
   const {user_id} = useUserState(state => state)
   console.log(user_id, post_id)
   console.log("commenting")
   return (
     <div className={"h-screen w-screen fixed top-0 left-0 bg-black/50 backdrop-blur-xs text-white"}>
-    <div className={"mxx-auto sm:w-96 w-screen h-screen py-24"}>
+    <div className={"mx-auto sm:w-96 w-screen h-screen py-24"}>
       <div className={"bg-green-400/20"}>
         New reply to post {post_id} from user {user_id}
       </div>
