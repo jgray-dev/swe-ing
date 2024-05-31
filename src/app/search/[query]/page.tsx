3e7df3e314c107c2f83ede36b2fd8d62db1e3045
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-31 04:27:11 +0000

    Add console logs and refactor useEffect in search/[query] component

--- src/app/search/[query]/page.tsx
+++ src/app/search/[query]/page.tsx
@@ -11,11 +11,13 @@ export default function SearchPage({ params }: { params: { query: string } }) {
   const { user_id } = useUserState((state) => state);
 
   useEffect(() => {
+    console.log("useEffect called")
     void firstLoad();
     //eslint-disable-next-line
-  }, [user_id]);
+  }, [user_id,]);
 
   async function firstLoad() {
+    console.log("firstLoad called")
     if (user_id) {
       const po = await searchEmbeddings(params.query);
       setPo(po);
@@ -25,12 +27,14 @@ export default function SearchPage({ params }: { params: { query: string } }) {
   }
 
   if (po) {
+    console.log("We have PO")
     return (
       <div className={"h-screen w-screen pt-20"}>
         <PostsPage order={po} />
       </div>
     );
   } else {
+    console.log("Waiting for PO")
     return (
       <div className={"w-screen h-screen text-center text-zinc-600 pt-20"}>
         <VscLoading className={"animate-roll mx-auto h-8 w-8 text-white"} />
