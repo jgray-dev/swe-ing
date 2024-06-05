ba57c2e0bc0b1aa6e3ba133c928fa2e11aac0d2d
Jackson Gray <nohaxjustdoge@gmail.com>
2024-06-05 00:26:36 +0000

    Refactor NavBar.tsx, fix import spacing, and add TfiReload icon import

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -3,7 +3,7 @@ import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import {updateEmbed} from "~/server/api/queries";
+import { updateEmbed } from "~/server/api/queries";
 
 import { IoHomeOutline } from "react-icons/io5";
 import { TfiReload } from "react-icons/tfi";
