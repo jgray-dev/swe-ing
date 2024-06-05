eaa93100373dedfd50088b1c651ed0ada8babc05
Jackson Gray <nohaxjustdoge@gmail.com>
2024-06-05 04:29:28 +0000

    Add alert components for displaying notifications and dismiss functionality

--- src/app/_functions/store.tsx
+++ src/app/_functions/store.tsx
@@ -4,6 +4,13 @@ import { create } from "zustand";
 import { useUser } from "@clerk/clerk-react";
 import React from "react";
 import { getDbUser } from "~/server/api/queries";
+import {
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+  XCircleIcon,
+  XMarkIcon,
+} from "@heroicons/react/20/solid";
+import { TailSpin } from "react-loading-icons";
 
 type User = {
   user_id: number;
@@ -58,6 +65,7 @@ export function UserDataUpdater() {
       }
     };
     void fetchData();
+    //eslint-disable-next-line
   }, [isLoaded, isSignedIn, user]);
 
   return null;
@@ -72,3 +80,103 @@ export const useReplyState = create<Reply>((set) => ({
   post_id: 0,
   setReplyData: (data) => set(data),
 }));
+
+type Alert = {
+  text: string;
+  type: "info" | "warn" | "error" | "loading" | "";
+  setAlert: (data: Partial<Alert>) => void;
+};
+
+export const useAlertState = create<Alert>((set) => ({
+  text: "",
+  type: "",
+  setAlert: (data) => set({ ...data }),
+}));
+
+export function Alert() {
+  const { text, type, setAlert } = useAlertState((state) => state);
+  const lastUpdateRef = React.useRef(Date.now());
+
+  React.useEffect(() => {
+    if (text !== "" && type !== "") {
+      lastUpdateRef.current = Date.now();
+    }
+  }, [text, type]);
+
+  React.useEffect(() => {
+    const checkTimeout = () => {
+      const timeElapsed = Date.now() - lastUpdateRef.current;
+      if (timeElapsed >= 10000) {
+        setAlert({ text: "" });
+      } else {
+        setTimeout(checkTimeout, 10000 - timeElapsed);
+      }
+    };
+    checkTimeout();
+    // @ts-expect-error fts
+    return () => clearTimeout(checkTimeout);
+  }, [setAlert]);
+
+  if (text === "" || type === "") return null;
+  const icons = {
+    info: (
+      <CheckCircleIcon className="h-5 w-5 text-green-500" aria-hidden="true" />
+    ),
+    warn: (
+      <ExclamationTriangleIcon
+        className="h-5 w-5 text-orange-500"
+        aria-hidden="true"
+      />
+    ),
+    error: <XCircleIcon className="h-5 w-5 text-red-500" aria-hidden="true" />,
+    loading: (
+      <TailSpin stroke="#0e7490" className="h-5 w-5" aria-hidden="true" />
+    ),
+  };
+
+  const bgcolor = {
+    info: "bg-green-100",
+    warn: "bg-orange-100",
+    error: "bg-red-100",
+    loading: "bg-cyan-100",
+    "": "",
+  };
+
+  const textcolor = {
+    info: "text-green-700",
+    warn: "text-orange-700",
+    error: "text-red-700",
+    loading: "text-cyan-700",
+    "": "",
+  };
+
+  return (
+    <div className="fixed bottom-10 right-10 z-50 h-fit shadow-xl shadow-black">
+      <div className={`rounded-md bg-green-50 ${bgcolor[type]} p-4`}>
+        <div className="flex">
+          <div className="flex-shrink-0">{icons[type]}</div>
+          <div className="ml-3">
+            <p className={`text-sm font-medium ${textcolor[type]}`}>
+              {text}
+            </p>
+          </div>
+          <div className="ml-auto pl-3">
+            <div className="-mx-1.5 -my-1.5">
+              <button
+                type="button"
+                className={`inline-flex rounded-md ${bgcolor[type]}  p-1.5 ${textcolor[type]} hover:bg-opacity-60`}
+              >
+                <span className="sr-only">Dismiss</span>
+                <XMarkIcon
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                  onClick={() => setAlert({text: ""})}
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
