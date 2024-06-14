7cf78fc7c80a0ec4b1b3faf50c4e431d2b298635
Jackson Gray <nohaxjustdoge@gmail.com>
2024-06-14 21:25:48 +0000

    Add KeepAlive component to periodically wake database and prevent timeouts

---
+++ src/app/_functions/database.ts
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+import { wakeDatabase } from "~/server/api/queries";
+
+
+export function KeepAlive() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      void wakeDatabase();
+    }, 58000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return null;
+}
