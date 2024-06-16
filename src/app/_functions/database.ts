f4d58d818b959d41782dce50307d5c2fd5111905
Jackson Gray <nohaxjustdoge@gmail.com>
2024-06-16 23:33:24 +0000

    Decrease database keep-alive interval from 55 seconds to 50 seconds

--- src/app/_functions/database.ts
+++ src/app/_functions/database.ts
@@ -7,7 +7,7 @@ export function KeepAlive() {
   useEffect(() => {
     const interval = setInterval(() => {
       void wakeDatabase();
-    }, 55000);
+    }, 50000);
     return () => clearInterval(interval);
   }, []);
 
