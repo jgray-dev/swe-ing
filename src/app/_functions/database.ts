2104356ea90e4cd1c94f53d01742a0191c6a3d4f
Jackson Gray <nohaxjustdoge@gmail.com>
2024-06-17 05:50:27 +0000

    Fix: remove console log statement, prevent database from sleeping

--- src/app/_functions/database.ts
+++ src/app/_functions/database.ts
@@ -6,7 +6,6 @@ import { wakeDatabase } from "~/server/api/queries";
 export function KeepAlive() {
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log("Waking database");
       void wakeDatabase();
     }, 50000);
     return () => clearInterval(interval);
