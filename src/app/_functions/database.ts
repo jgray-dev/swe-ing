c65ac1605507ab97736d7a6e9bd14d845444babb
Jackson Gray <nohaxjustdoge@gmail.com>
2024-06-17 03:49:19 +0000

    Update database.ts: Add console log for waking database and set interval

--- src/app/_functions/database.ts
+++ src/app/_functions/database.ts
@@ -6,10 +6,10 @@ import { wakeDatabase } from "~/server/api/queries";
 export function KeepAlive() {
   useEffect(() => {
     const interval = setInterval(() => {
+      console.log("Waking database")
       void wakeDatabase();
     }, 50000);
     return () => clearInterval(interval);
   }, []);
-
   return null;
 }
